b68bfe41f2348a3b358842b3b435f36e94f8b567
ShahafRSeza <sezashahaf@gmail.com>
2022-08-30 19:44:34 +0300

    .

--- recipesApp/src/app/app.module.ts
+++ recipesApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { ShowRecipesComponent } from './components/show-recipes/show-recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ShowRecipesComponent
   ],
   imports: [
     BrowserModule,
---
+++ recipesApp/src/app/components/show-recipes/show-recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-recipes',
+  templateUrl: './show-recipes.component.html',
+  styleUrls: ['./show-recipes.component.css']
+})
+export class ShowRecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
